19e66997e55946f68aa7944df7e81efd6d5f97a6
CDEguia <candelario.eguia@laverne.edu>
2016-06-03 22:52:59 -0700

    added mouse detection to screen

--- javascripts/script.js
+++ javascripts/script.js
@@ -2,17 +2,25 @@ function winResize() {
   $('#home').height(window.innerHeight);
   $('#connect').height(window.innerHeight);
 }
-$(document).ready(function(){
+function showCoords(event) {
+    var y = event.clientY;
+    var isHidden = $('#navgation').is(':visible');
+    console.log(isHidden)
+    if((y >=0 && y<=15) && isHidden != true){
+        window.scrollBy(0, -25);
+    }
+}
+$(document).ready(function () {
   winResize();
 });
 
 
-$(window).resize(function() {
+$(window).resize(function () {
   winResize();
 });
 
 // handle links with @href started with '#' only
-$(document).on('click', 'a[href^="#"]', function(e) {
+$(document).on('click', 'a[href^="#"]', function (e) {
   var id = $(this).attr('href'); // target element id
 
   var $id = $(id); // target element
@@ -30,21 +38,18 @@ $(document).on('click', 'a[href^="#"]', function(e) {
 });
 var previous_scroll = $(window).scrollTop();
 
-$(window).on('scroll', function() {
+$(window).on('scroll', function () {
   var scroll = $(window).scrollTop(),
-    scroll_change = scroll - previous_scroll;
+      scroll_change = scroll - previous_scroll;
   previous_scroll = scroll;
   $(window).trigger('custom_scroll', [scroll_change]);
 });
 $(window).on('custom_scroll', function pos(e, scroll_change) {
-   //if($('#navgation').css("display") != 'none'){
+  //if($('#navgation').css("display") != 'none'){
   if (scroll_change > 0) {
-   
-    
-    $('#navgation').slideUp("slow");
-  //  $('#navbardetection').style.display = 'block'; //document.getElementById('navgation').style.display = 'none';
+    $('#show').slideDown("slow");
+    //  $('#navbardetection').style.display = 'block'; //document.getElementById('navgation').style.display = 'none';
   } else {
-   // $('#navgation').show("slow");
-   $('#navgation').slideDown("slow"); //document.getElementById('navgation').style.display = 'block';
+    $('#navgation').slideDown("slow"); //document.getElementById('navgation').style.display = 'block';
   }
 });
